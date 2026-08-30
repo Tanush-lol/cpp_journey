da7bcec4bf47295f354db3f2e387b6e75d1bf092
Tanush-lol <tanushroy1@gmail.com>
2026-08-30 16:03:06 +0530

    woah

--- constructors.cpp
+++ constructors.cpp
@@ -1,31 +1,19 @@
 #include <iostream>
 
-class idk{
-  private:
-    const char *b;
-  public:
-    const char *number;
-    
-    void l(){
-      std::cout<<"called "<<number<<'\n';
-    }
-    idk (const char *x :b(move(x))){
-      cout<<"Move constructor called"<<endl;
-    }
-    idk(const char *x){//parameterized constructor
-      std::cout<<"constructor called"<<'\n';
-      number = x;
-    }
-    idk(idk& nigger){
-      number = nigger.number;// see that the number is copied from the value and then added over here
-    }
+class Box{
+  //defualt constructor
+  Box(){}
+  
+  //init a cube
+  explicit Box(int i) : m_width(i), m_length(i), m_height(i) // member init list
+    {}
+
+  //init a box with custom dimensions
+  Box(int width,int length,int height):m_width(width),m_length(length), m_height(height)
+  {}
+
 };
 
 int main(){
-  idk a("hello world");
-  a.l();
-  idk a1(a);
-  a1.l();
-  a1(move(a));
   return 0;
 }
--- moveConstructor.cpp
+++ moveConstructor.cpp
@@ -2,9 +2,7 @@
 // basically a moveconstructor moves rvalue to an lvalue
 
 class MemoryBlock{
-  MemoryBlock(MemoryBlock&& other: _data(nullptr)){
 
-  }
 };
 
 int main(){
