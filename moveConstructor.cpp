1878b183098c2a2a2ce93c021b2ba546fa33d43d
Tanush-lol <tanushroy1@gmail.com>
2026-09-19 12:48:45 +0530

    move assignment

--- moveConstructor.cpp
+++ moveConstructor.cpp
@@ -51,13 +51,43 @@ class Smthng{
       std::cout<<"move constructor was called "<<'\n';
     }
 
+    Smthng& operator = (Smthng&& other){
+      std::cout<<"move assigment called"<<'\n';
+   if (this != &other)
+   {
+      // Free the existing resource.
+      delete[] _data;
+
+      // Copy the data pointer and its length from the
+      // source object.
+      _data = other._data;
+      _length = other._length;
+
+      // Release the data pointer from the source object so that
+      // the destructor does not free the memory multiple times.
+      other._data = nullptr;
+      other._length = 0;
+   }
+   return *this;
+    }
+
+
+
 
 };
 
 int main(){
 
   Smthng s1(10);
-  Smthng s2 = std::move(s1);
+  Smthng s2(5);
+  s2 = std::move(s1); //is wrong 
+//   //What std::move(s1) actually does
+// It basically tells C++:
+// "Treat s1 as something whose resources can be moved from."
+// So:
+// s2 = std::move(s1);
+// makes C++ look for:
+// Smthng& operator=(Smthng&& other);
 
 
   return 0;
